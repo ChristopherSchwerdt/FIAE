b0e59e4e936713b2cf0b661e7323721383ae05c7
ChristopherSchwerdt <christopher.schwerdt1988@gmail.com>
2024-02-05 08:56:38 +0100

    kommentare

--- Unterrichtsmaterialien/Python Crashkurs/3_GoogleMaps.py
+++ Unterrichtsmaterialien/Python Crashkurs/3_GoogleMaps.py
@@ -1,8 +1,14 @@
+# Dieses Python-Programm öffnet Google-Maps mit der per Kommandozeilenparameter 
+# übergebenen Adresse oder alternativ per Zwischenablage (STRG+C)
+
 import webbrowser,sys,pyperclip
 
+#Wenn etwas per Kommandozeilenparameter übergeben worden ist:
 if len(sys.argv) > 1:
+    # ... alle Parameter aus dem Array in einen String zusammenfassen.
     address = ' '.join(sys.argv[1:])
 else:
+    #Ansonsten nimm die Adresse aus der Zwischenablage(STRG+C).
     address = pyperclip.paste()
     
 webbrowser.open("https://google.de/maps/place/" + address)

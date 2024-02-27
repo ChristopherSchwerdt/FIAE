c1b3cc78df2175d55f1c89fa84368d45348b6ffc
ChristopherSchwerdt <christopher.schwerdt1988@gmail.com>
2024-02-27 11:39:50 +0100

    added stuff

---
+++ Unterrichtsmaterialien/C-Sharp/2. weitere Konsolenprogramme/Zahlenraten/Zahlenraten/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Zahlenraten
+{
+    class Program
+    {
+        static Random myRandom = new Random();
+        static int number;
+        static int currentNumber;
+        static void Main(string[] args)
+        {
+          
+            //1. eine Zufallszahl zwischen 1 und 1000
+            number = myRandom.Next(1,1000);
+            while(true)
+            {
+                //2. eine Eingabe vom User
+                Console.WriteLine("Gib eine Zahl zwischen 1 und 1000 ein:");
+                currentNumber = int.Parse(Console.ReadLine());
+                //3. prüfen der Zahl gegen Suchzahl
+                if (currentNumber == number)
+                {
+                    break;
+                }
+                //4. Ausgabe ob Zahl größer / kleiner
+                if (currentNumber > number)
+                {
+                    Console.WriteLine("Die gesuchte Zahl ist kleiner!");
+                }
+                if (currentNumber < number)
+                {
+                    Console.WriteLine("Die gesuchte Zahl ist größer!");
+                }
+
+            }
+            Console.WriteLine("Zahl gefunden! :) ");
+
+
+
+        }
+
+    }
+
+}

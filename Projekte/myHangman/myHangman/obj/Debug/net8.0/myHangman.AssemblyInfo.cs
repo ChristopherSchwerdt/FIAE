7cfb4ab1255b5acb36f94e94dcafdb23995cd3d1
ChristopherSchwerdt <christopher.schwerdt1988@gmail.com>
2024-08-20 13:44:17 +0200

    hinzugefügt

--- Projekte/myHangman/myHangman/obj/Debug/net8.0/myHangman.AssemblyInfo.cs
+++ Projekte/myHangman/myHangman/obj/Debug/net8.0/myHangman.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myHangman")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0c27e169aca78ab6b4c295647ef93a1e90a1b4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("myHangman")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myHangman")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Unterrichtsmaterialien/C-Sharp/02. weitere Konsolenprogramme/myEncrypt/myEncrypt/obj/Debug/net8.0/myEncrypt.AssemblyInfo.cs
+++ Unterrichtsmaterialien/C-Sharp/02. weitere Konsolenprogramme/myEncrypt/myEncrypt/obj/Debug/net8.0/myEncrypt.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myEncrypt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0c27e169aca78ab6b4c295647ef93a1e90a1b4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("myEncrypt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myEncrypt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
